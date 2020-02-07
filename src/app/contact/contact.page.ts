5f4ac2c7e6d79138de3d474717c30838ead8effc
lpozo1990 <lpozo1990@gmail.com>
2020-02-07 17:16:07 -0500

    Modified contacts section

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -8,12 +8,12 @@ import { CallNumber } from '@ionic-native/call-number/ngx';
   styleUrls: ['./contact.page.scss'],
 })
 export class ContactPage implements OnInit {
-
   constructor(private callNumber: CallNumber) { }
-  call() {
-    this.callNumber.callNumber('5358766375', true)
+
+  call(number:string) {
+    this.callNumber.callNumber(number, true)
       .then(res => console.log('Launched dialer!', res))
-      .catch(err => console.log('Error launching dialer', err));
+      .catch(err => console.log('Error launching dialer', err, number));
   }
 
   ngOnInit() {
